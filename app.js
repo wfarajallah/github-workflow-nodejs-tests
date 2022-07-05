5aa03d1f2d3e4faa936dd98733647ba1bd8a7cc0
wfarajallah <wfarajallah@gmail.com>
2022-07-05 04:38:28 +0400

    updaing hello world msg

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 80
 
 app.get('/', (req, res) => {
-  res.send('Hello World! ^^')
+  res.send('Hello World! .. in GitHub Actions')
 })
 
 app.listen(port, () => {
